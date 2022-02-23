02236bcb1c66cebfd44a1b448aa5035c91c4496b
akatukikyuto_ <akatukikyuto@gmail.com>
2022-02-24 08:31:33 +0900

    初期

---
+++ MC_DTG/MainForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace MC_DTG
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ExecutionButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.NamespaceTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.DirectoryButton = new System.Windows.Forms.Button();
+            this.VersionComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // ExecutionButton
+            // 
+            resources.ApplyResources(this.ExecutionButton, "ExecutionButton");
+            this.ExecutionButton.Name = "ExecutionButton";
+            this.ExecutionButton.UseVisualStyleBackColor = true;
+            this.ExecutionButton.Click += new System.EventHandler(this.ExecutionButton_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // NameTextBox
+            // 
+            resources.ApplyResources(this.NameTextBox, "NameTextBox");
+            this.NameTextBox.Name = "NameTextBox";
+            // 
+            // NamespaceTextBox
+            // 
+            resources.ApplyResources(this.NamespaceTextBox, "NamespaceTextBox");
+            this.NamespaceTextBox.Name = "NamespaceTextBox";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // DescriptionTextBox
+            // 
+            resources.ApplyResources(this.DescriptionTextBox, "DescriptionTextBox");
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // DirectoryTextBox
+            // 
+            resources.ApplyResources(this.DirectoryTextBox, "DirectoryTextBox");
+            this.DirectoryTextBox.Name = "DirectoryTextBox";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // DirectoryButton
+            // 
+            resources.ApplyResources(this.DirectoryButton, "DirectoryButton");
+            this.DirectoryButton.Name = "DirectoryButton";
+            this.DirectoryButton.UseVisualStyleBackColor = true;
+            this.DirectoryButton.Click += new System.EventHandler(this.DirectoryButton_Click);
+            // 
+            // VersionComboBox
+            // 
+            this.VersionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.VersionComboBox.FormattingEnabled = true;
+            this.VersionComboBox.Items.AddRange(new object[] {
+            resources.GetString("VersionComboBox.Items"),
+            resources.GetString("VersionComboBox.Items1"),
+            resources.GetString("VersionComboBox.Items2"),
+            resources.GetString("VersionComboBox.Items3"),
+            resources.GetString("VersionComboBox.Items4")});
+            resources.ApplyResources(this.VersionComboBox, "VersionComboBox");
+            this.VersionComboBox.Name = "VersionComboBox";
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.VersionComboBox);
+            this.Controls.Add(this.DirectoryButton);
+            this.Controls.Add(this.DirectoryTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.DescriptionTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NamespaceTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ExecutionButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExecutionButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox NamespaceTextBox;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+        private System.Windows.Forms.TextBox DirectoryTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button DirectoryButton;
+        private System.Windows.Forms.ComboBox VersionComboBox;
+    }
+}
+
---
+++ MC_DTG/MainForm.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Runtime.Serialization.Json;
+
+namespace MC_DTG
+{
+    public partial class MainForm : Form
+    {
+        private bool FirstTime = false;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            FirstTime = false;
+        }
+
+        public class Datapack
+        {
+            public string Name = "";
+            public string Namespace = "";
+            public int PackFormat = 0;
+            public string Description = "";
+        }
+
+        [System.Runtime.Serialization.DataContract]
+        public class Pack
+        {
+            [System.Runtime.Serialization.DataMember()]
+            public int Pack_format { get; set; }
+
+            [System.Runtime.Serialization.DataMember()]
+            public string Description { get; set; }
+        }
+
+        [System.Runtime.Serialization.DataContract]
+        public class MCMeta
+        {
+            [System.Runtime.Serialization.DataMember()]
+            public Pack Pack { get; set; }
+        }
+
+        [System.Runtime.Serialization.DataContract]
+        public class Json
+        {
+            [System.Runtime.Serialization.DataMember()]
+            public List<string> Values { get; set; }
+        }
+
+        private readonly Datapack datapack = new Datapack();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExecutionButton_Click(object sender, EventArgs e)
+        {
+            if (VersionComboBox.SelectedIndex == -1) VersionComboBox.SelectedIndex = VersionComboBox.Items.Count - 1;
+            CreatePack();
+        }
+
+        private void CreatePack()
+        {
+            try
+            {
+                //データセット
+                datapack.Name = NameTextBox.Text;
+                datapack.Namespace = NamespaceTextBox.Text;
+                datapack.Description = DescriptionTextBox.Text;
+                _ = int.TryParse(VersionComboBox.SelectedItem.ToString()[0].ToString(), out datapack.PackFormat);
+
+                bool isEnd = false;
+                if (datapack.Name == "")
+                {
+                    _ = MessageBox.Show("エラーが発生しました\nデータパック名の入力に不備があります");
+                    isEnd = true;
+                }
+                else if (datapack.Namespace == "")
+                {
+                    _ = MessageBox.Show("エラーが発生しました\n名前空間の入力に不備があります");
+                    isEnd = true;
+                }
+                else if (DirectoryTextBox.Text == "")
+                {
+                    _ = MessageBox.Show("エラーが発生しました\n保存場所の入力に不備があります");
+                    isEnd = true;
+                }
+                if (isEnd) return;
+                
+                //:保存先
+                var path = $"{DirectoryTextBox.Text}\\{datapack.Name}";
+                _ = Directory.CreateDirectory(path);
+
+                //:保存先\<データパック名>
+                var pack = new Pack()
+                {
+                    Pack_format = datapack.PackFormat,
+                    Description = datapack.Description
+                };
+                var mcmeta = new MCMeta()
+                {
+                    Pack = pack
+                };
+                CreateFile(path, "pack.mcmeta", typeof(MCMeta), mcmeta);
+                path += "\\data";
+
+                //:保存先\<データパック名>\data
+                var path_minecraft = $"{path}\\minecraft\\tags\\functions";
+                var path_namespace = $"{path}\\{datapack.Namespace}";
+                _ = Directory.CreateDirectory(path_minecraft);
+                _ = Directory.CreateDirectory(path_namespace);
+
+                //:保存先\<データパック名>\data\minecraft\tags\functions
+                var strings = new string[2]
+                {
+                    "load","tick"
+                };
+                foreach (var s in strings)
+                {
+                    var list = new List<string>()
+                    {
+                        $"{datapack.Namespace}:{s}"
+                    };
+                    var json = new Json()
+                    {
+                        Values = list
+                    };
+                    CreateFile(path_minecraft, $"{s}.json", typeof(Json), json);
+                }
+
+                //:保存先\<データパック名>\data\<名前空間>
+                var namespace_under = new string[11]
+                {
+                    "advancements","functions","item_modifiers","loot_tables","predicates","recipes","structures","tags","dimension_type","dimension","worldgen"
+                };
+                foreach (string s in namespace_under)
+                {
+                    _ = Directory.CreateDirectory($"{path_namespace}\\{s}");
+                }
+
+                //:保存先\<データパック名>\data\<名前空間>\tags
+                var path_tags = $"{path_namespace}\\tags";
+                var tags_under = new string[5]
+                {
+                    "blocks","entity_types","fluids","functions","items"
+                };
+                foreach (string s in tags_under)
+                {
+                    _ = Directory.CreateDirectory($"{path_tags}\\{s}");
+                }
+
+                //:保存先\<データパック名>\data\<名前空間>\worldgen
+                var path_worldgen = $"{path_namespace}\\worldgen";
+                var worldgen_under = new string[8]
+                {
+                    "biome","configured_carver","configured_feature","configured_structure_feature","configured_surface_builder","noise_settings","processor_list","template_pool"
+                };
+                foreach (var s in worldgen_under)
+                {
+                    _ = Directory.CreateDirectory($"{path_worldgen}\\{s}");
+                }
+
+                _ = MessageBox.Show("生成が正常に完了しました");
+            }
+            catch (Exception ex)
+            {
+                _ = MessageBox.Show($"エラーが発生しました\n{ex}");
+                return;
+            }
+        }
+
+        private void CreateFile(string path, string name, Type type, dynamic dynamic)
+        {
+            //使い方:CreateFile(path,name,typeof(Type),text);
+            //path:フォルダパス
+            //name:ファイル名
+            //typeof(Type):データタイプ
+            //text:テキストデータ
+            using (var fs = new FileStream($"{path}\\{name}", FileMode.Create))
+            using (var writer = JsonReaderWriterFactory.CreateJsonWriter(fs, Encoding.UTF8, true, true, "    "))
+            {
+                var serializer = new DataContractJsonSerializer(type);
+                serializer.WriteObject(writer, dynamic);
+            }
+        }
+
+        private void DirectoryButton_Click(object sender, EventArgs e)
+        {
+            if (!FirstTime)
+            {
+                FirstTime = true;
+                using (var cofd = new CommonOpenFileDialog()
+                {
+                    Title = "フォルダを選択してください",
+                    InitialDirectory = Directory.GetCurrentDirectory(),
+                    IsFolderPicker = true,
+                })
+                {
+                    if (cofd.ShowDialog() != CommonFileDialogResult.Ok)
+                    {
+                        return;
+                    }
+                    DirectoryTextBox.Text = cofd.FileName;
+                }
+            }
+            else
+            {
+                using (var cofd = new CommonOpenFileDialog()
+                {
+                    Title = "フォルダを選択してください",
+                    RestoreDirectory = true,
+                    IsFolderPicker = true,
+                })
+                {
+                    if (cofd.ShowDialog() != CommonFileDialogResult.Ok)
+                    {
+                        return;
+                    }
+                    DirectoryTextBox.Text = cofd.FileName;
+                }
+            }
+        }
+    }
+}
